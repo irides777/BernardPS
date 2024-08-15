96c4764af21831c2856da3aded06a541c1e06e91
irides <irides777@gmail.com>
2024-08-16 00:26:59 +0800

    undate task server

--- bernard/server/schedule/datetime.py
+++ bernard/server/schedule/datetime.py
@@ -20,7 +20,7 @@ class WeekdayCalSig(dspy.Signature):
     date_delta = dspy.OutputField(desc="An integer, Number of days between current date and relative weekday or date. NO EXPLANATION NEEDED.")
 
 relative_date_cal = dspy.ChainOfThought(WeekdayCalSig)
-relative_date_cal.load('/home/irides/project/secretary/bernard/server/schedule/model.json')
+relative_date_cal.load(r"D:\projects\BernardPS\bernard\server\schedule\model.json")
 
 def process_raw_date(raw_date: str) -> str:
     if type(raw_date) is dt.date or re.compile(r"^\d{4}-\d{2}-\d{2}$").match(raw_date):
--- dspy_demo.py
+++ dspy_demo.py
@@ -12,14 +12,28 @@ import asyncio
 from bernard.router import DialogueRouter
 from bernard.server import ScheduleEventServer, GeneralChatServer, ReminderServer, TaskServer
 from bernard.channel import Channel
-from bernard.ui import CmdUI
+from bernard.ui import CmdUI, WxautoUI
+
+from wxauto import WeChat
+
+from dotenv import load_dotenv, find_dotenv
+
+# 一、自动搜索 .env 文件
+load_dotenv(verbose=True)
 
 
-cmd_ui = CmdUI()
-channel = Channel(ui=cmd_ui)
+# cmd_ui = CmdUI()
+wx = WeChat()
+wx_ui = WxautoUI(wx_window=wx, user_name='irides')
+channel = Channel(ui=wx_ui)
 channel.router.add_server('Create Time Reminder', ReminderServer(channel))
 channel.router.add_server('Create Task', TaskServer(channel))
 channel.router.add_server('Chat', GeneralChatServer(channel))
+
+wx_ui2 = WxautoUI(wx_window=wx, user_name='123')
+channel2 = Channel(ui=wx_ui2)
+channel2.router.add_server('Create Time Reminder', ReminderServer(channel2))
+channel2.router.add_server('Chat', GeneralChatServer(channel2))
 
 lm = dspy.OpenAI(model='qwen-plus', api_base='https://dashscope.aliyuncs.com/compatible-mode/v1', api_key=os.getenv('DASHSCOPE_API_KEY'), model_type='chat')
 dspy.settings.configure(lm=lm)
@@ -31,11 +45,16 @@ dspy.settings.configure(lm=lm)
 #     dialogue: dspy.Dialogue = dspy.InputField(desc="Dialogue consists of role, content and time")
 #     relative_date: dspy.Date = dspy.OutputField(desc="the relative date user want to set. If there is insuffcient information to fill a field, just fill it with 'unknown'.")
 
+async def main():
+    task1 = asyncio.create_task(channel.run())
+    task2 = asyncio.create_task(channel2.run())
+    await asyncio.gather(task1, task2)
+
 
 if __name__ == '__main__':
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    asyncio.run(main())
     # teleprompter = BootstrapFewShot(metric=validate_answer)
     # optimized_program = teleprompter.compile(pred, trainset=trainset)
     # optimized_program.save('model.json')
     # print(optimized_program(current_weekday='Sat', relative_weekday_or_date='下下周五').date_delta)
-    asyncio.run(channel.run())
